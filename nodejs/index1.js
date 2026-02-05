9082d9ce3c5bbd717e8b23429c7769e9baae4081
anni <aniketsharma2412@gmail.com>
2026-02-05 12:21:28 +0530

    commit

--- nodejs/index1.js
+++ nodejs/index1.js
@@ -1,2 +1,5 @@
+//const fs = require('fs');
+//fs.cpSync("IT-A.txt","CE-A.txt");
+
 const fs = require('fs');
-fs.cpSync("IT-A.txt","CE-A.txt");
+fs.appendFileSync("IT-A.txt","and we are coders\n");
