8d15190ebd443e659d2bbdfe46936b9f69c532ba
anni <aniketsharma2412@gmail.com>
2026-02-05 12:38:36 +0530

    done

--- nodejs/index1.js
+++ nodejs/index1.js
@@ -1,5 +1,14 @@
 //const fs = require('fs');
 //fs.cpSync("IT-A.txt","CE-A.txt");
 
-const fs = require('fs');
-fs.appendFileSync("IT-A.txt","and we are coders\n");
+//const fs = require('fs');
+//fs.appendFileSync("IT-A.txt","and we are coders\n");
+
+//const fs = require('fs');
+//fs.unlinkSync("CE-A.txt");
+
+const os = require('os');
+console.log("Architecture: " + os.arch());
+console.log("Number of CPUs: " + os.cpus().length);
+console.log("Free Memory: " + os.freemem());
+console.log("Total Memory: " + os.totalmem());
